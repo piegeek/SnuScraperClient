104672f6e436f880fbcfde076507ea22b9966cdf
piegeek <piegeek2@naver.com>
2020-01-18 02:09:49 +0900

    Connected to firebase

--- android/app/src/main/java/com/snuscraperclient/MainApplication.java
+++ android/app/src/main/java/com/snuscraperclient/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseMessagingPackage());
       return packages;
     }
 
